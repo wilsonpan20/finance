21ebe3bb13b11128bea8baf5b2975407983339ef
wilsonpan20 <wilson.evangelista22@gmail.com>
2023-01-15 10:57:26 -0300

    delegação schema

---
+++ vue-finance-back/src/index.js
@@ -0,0 +1,20 @@
+const {prisma} = require('./generated/prisma-client')
+const Binding = require('prisma-binding')
+const { GraphQLServer } = require('graphql-yoga')
+
+const resolvers = require('./resolvers')
+
+const server = new GraphQLServer({
+  typeDefs:`${__dirname}/schema.graphql`,
+  resolvers,
+  context:{
+    db:new Binding.Prisma({
+      typeDefs:`${__dirname}/generated/graphql-schema/prisma.graphql`,
+      endpoint: process.env.PRISMA_ENDPOINT
+    }),
+    prisma
+  }
+  
+})
+
+server.start().then(()=> console.log('server running on http://localhost:4000...'))
