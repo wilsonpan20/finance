a2dc7664274e300735c7b9ee3be96805db308430
wilsonpan20 <wilson.evangelista22@gmail.com>
2023-02-05 18:08:14 -0300

    Salvado o token no usuario no localstorage

--- vue-finances-front/src/plugins/apollo.js
+++ vue-finances-front/src/plugins/apollo.js
@@ -1,4 +1,18 @@
 import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost'
+const AUTH_TOKEN = 'apollo-token'
+const resetApolloClient = async (apollo) => {
+  try {
+    await apollo.resetStore()
+  } catch (error) {
+    console.log('%Error on cache reset', 'color: orange;', error.message)
+  }
+}
+const onLogin = async (apollo, token) => {
+  if (typeof window.localStorage !== 'undefined' && token) {
+    window.localStorage.setItem(AUTH_TOKEN, token)
+  }
+  await resetApolloClient(apollo)
+}
 
 const link = new HttpLink({
   uri: 'http://localhost:4000'
@@ -10,3 +24,5 @@ const apollo = new ApolloClient({
 })
 
 export default apollo
+
+export { AUTH_TOKEN, onLogin }
