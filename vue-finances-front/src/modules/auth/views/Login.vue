4c8aceb004f4ade282e82c04c327c9090a32b2ce
wilsonpan20 <wilson.evangelista22@gmail.com>
2023-02-05 16:34:53 -0300

    validando o formulario de login e signup

--- vue-finances-front/src/modules/auth/views/Login.vue
+++ vue-finances-front/src/modules/auth/views/Login.vue
@@ -3,22 +3,29 @@
     <v-layout justify-center align-center>
       <v-flex xs12 sm6 md4 lg3>
         <v-card class="elevation-12">
-          <v-toolbar color="deep-purple accent-4" dark>
-            Login
+          <v-toolbar color="green darken-4" dark>
+            <v-toolbar-title>{{ texts.toolbar }}</v-toolbar-title>
           </v-toolbar>
           <v-card-text>
             <v-form>
-              <v-text-field prepend-icon="email" name="email" label="Email" type="email"></v-text-field>
-              <v-text-field prepend-icon="lock" name="password" label="Senha" type="password"></v-text-field>
+              <v-text-field v-if="!isLogin" v-model.trim="$v.user.name.$model" :success="!$v.user.name.$invalid"
+                prepend-icon="person" name="name" label="Nome" type="text " :error-messages="nameErrors"></v-text-field>
+              <v-text-field prepend-icon="email" name="email" label="Email" type="email "
+                v-model.trim="$v.user.email.$model" :success="!$v.user.email.$invalid"
+                :error-messages="emailErrors"></v-text-field>
+              <v-text-field prepend-icon="lock" name="password" label="Senha" :error-messages="passwordErrors"
+                type="password" :success="!$v.user.password.$invalid"
+                v-model.trim="$v.user.password.$model"></v-text-field>
             </v-form>
-            <v-btn block depressed color="secondary">
-              Criar Conta
+
+            <v-btn block depressed color="secondary" @click="isLogin = !isLogin">
+              {{ texts.button }}
             </v-btn>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="deep-purple accent-4" large>
-              Login
+            <v-btn color="green darken-4" large :disabled="$v.$invalid" @click="submit">
+              {{ texts.toolbar }}
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -28,8 +35,82 @@
 </template>
 
 <script>
+import { required, email, minLength } from 'vuelidate/lib/validators'
 export default {
-  name: 'LoginUser'
+  name: 'LoginUser',
+  data: () => ({
+    isLogin: true,
+    user: {
+      name: '',
+      email: '',
+      password: ''
+    }
+
+  }),
+  validations () {
+    const validations = {
+      user: {
+        email: {
+          required,
+          email
+        },
+        password: {
+          required,
+          minLength: minLength(6)
+        }
+      }
+    }
+    if (this.isLogin) { return validations }
+    return {
+      user: {
+        ...validations.user,
+        name: {
+          required,
+          minLength: minLength(3)
+        }
+      }
+    }
+  },
+  computed: {
+    texts () {
+      return this.isLogin ? { toolbar: 'Entrar', button: 'Criar Conta' } : { toolbar: ' Criar Conta', button: 'Já tenho uma conta' }
+    },
+    nameErrors () {
+      const errors = []
+      const name = this.$v.user.name
+      if (!name.$dirty) { return errors }
+      !name.required && errors.push('Nome é obrigatório')
+      !name.minLength && errors.push(`Insira pelo menos ${name.$params.minLength.min} `)
+
+      return errors
+    },
+    emailErrors () {
+      const errors = []
+      const email = this.$v.user.email
+      if (!email.$dirty) { return errors }
+      !email.required && errors.push('Email é obrigatório')
+      !email.email && errors.push('Insira um email válido')
+
+      return errors
+    },
+    passwordErrors () {
+      const errors = []
+      const password = this.$v.user.password
+      if (!password.$dirty) { return errors }
+      !password.required && errors.push('Senha é obrigatório')
+      !password.minLength && errors.push(`Insira pelo menos ${password.$params.minLength.min} `)
+
+      return errors
+    }
+  },
+  methods: {
+    log () {
+      console.log('Vuelidate', this.$v)
+    },
+    submit () {
+      console.log('User', this.user)
+    }
+  }
 
 }
 </script>
