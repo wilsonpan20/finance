bcaba50b9a5523fbc50dc724d3db25326472ddb2
wilsonpan20 <wilson.evangelista22@gmail.com>
2023-02-13 22:05:55 -0300

    Criando o menu do app

--- vue-finances-front/src/modules/dashboard/components/AppLayout.vue
+++ vue-finances-front/src/modules/dashboard/components/AppLayout.vue
@@ -1,12 +1,19 @@
 <template>
-  <app-toobar />
+  <div>
+    <app-toobar v-model="drawer" />
+    <app-menu v-model="drawer" />
+  </div>
 </template> 
 
 <script>
+import AppMenu from './AppMenu.vue'
 import AppToobar from './AppToobar.vue'
 export default {
   name: 'AppLayout',
-  components: { AppToobar }
+  components: { AppToobar, AppMenu },
+  data: () => ({
+    drawer: false
+  })
 }
 
-</script>
+</script> 
---
+++ vue-finances-front/src/modules/dashboard/components/AppMenu.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-navigation-drawer :value="value" @input="$emit('input', $event)" :mini-variant="mini" absolute temporary>
+    <v-list>
+      <v-list-tile v-if="mini" @click.stop="mini = !mini">
+        <v-list-tile-action>
+          <v-icon>chevron_right</v-icon>
+        </v-list-tile-action>
+      </v-list-tile>
+      <v-list-tile avatar tag="div">
+        <v-list-tile-avatar>
+          <v-icon>person</v-icon>
+        </v-list-tile-avatar>
+        <v-list-tile-content>
+          <v-list-tile-title>
+            User Name
+          </v-list-tile-title>
+        </v-list-tile-content>
+        <v-list-tile-action>
+          <v-btn icon @click.stop="mini = !mini">
+            <v-icon>chevron_left</v-icon>
+          </v-btn>
+        </v-list-tile-action>
+      </v-list-tile>
+    </v-list>
+    <v-list class="pt-0" dense>
+      <v-divider light></v-divider>
+      <v-list-tile v-for="item in items" :key="item.title" :to="item.url" :exact="item.exact"
+        @click.stop="$emit('input', false)">
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'AppMenu',
+  props: {
+    value: Boolean
+  },
+  data: () => ({
+    items: [
+      { title: 'Home', icon: 'dashboard', url: '/dashboard', exact: true }
+    ],
+    mini: false
+  })
+}
+
+</script>
